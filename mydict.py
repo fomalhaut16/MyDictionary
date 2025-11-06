e0b3e13ee21c965cd32e12c3d837705961f4589a
Fomalhaut <fomalhaut2622@gmail.com>
2025-11-06 12:02:46 +0900

    fix: safer database url check for sqlite

--- mydict.py
+++ mydict.py
@@ -13,7 +13,7 @@ from fastapi.staticfiles import StaticFiles
 import os
 
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./terms.db")
-if "sqlite" in DATABASE_URL:
+if DATABASE_URL.startswith("sqlite"):
     engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 else:
     engine = create_engine(DATABASE_URL)
